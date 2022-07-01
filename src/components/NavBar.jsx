6a5dc027faff1b0ce38c0afbd90a950da9df4a5a
Milagros Abril Ruiz <abriferrin11@gmail.com>
2022-06-30 21:47:30 -0300

    agregue navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const NavBar = () => {
+    return ( 
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Profile</li>
+        </ul>
+    </nav>
+    );
+}
+ 
+export default NavBar;
