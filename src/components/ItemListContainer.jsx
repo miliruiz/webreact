4c0314a79a727b980997e2ad329b1c9fb9a79603
Milagros Abril Ruiz <abriferrin11@gmail.com>
2022-07-12 00:44:23 -0300

    props

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ItemCount from "./ItemCount"
 
-const ItemListContainer = ({ texto }) => {
+const ItemListContainer = (props) => {
 
     const onAdd = (quantity) => {
         console.log(`Compraste ${quantity} unidades`);
     }
 
     return (
+        <div>
+        <button>{props.greeting}</button>
         <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </div>
     );
 }
 
