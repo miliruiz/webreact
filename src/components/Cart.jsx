767c03c04b727ab9b7b3ef2719c4217c6971b92f
Milagros Abril Ruiz <abriferrin11@gmail.com>
2022-08-09 23:15:52 -0300

    orders

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,28 @@ import { Link } from "react-router-dom";
 import { useCartContext } from "./context/CartContext";
 import ItemCart from "./ItemCart";
 import "../styles/Cart.css"
+import { getFirestore, collection, getDocs, query, where, addDoc } from "firebase/firestore"
 
 const Cart = () => {
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            nombre: "Mili",
+            email: "abriferrin11@gmail.com",
+            phone: "2994625734",
+            direccion: "san martin 546"
+        },
+        items: cart.map(products => ({id: products.id, titulo:  products.titulo, precio: products.precio, quantity: products.quantity })),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const orderCollection = collection(db, "orders");
+        addDoc(orderCollection, order)
+        .then(({id}) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -26,6 +45,7 @@ const Cart = () => {
         <p className="total">
             total: ${totalPrice()}
         </p>
+        <button onClick={handleClick}>Comprar</button>
         </>
      );
 }
